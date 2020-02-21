b64b26e4c626704a2abefbce7089866ea8037459
Lucia Valachová <56638844+LuciaValachova@users.noreply.github.com>
2020-02-21 16:24:28 +0100

    novy subor

--- eshop.js
+++ eshop.js
@@ -156,3 +156,33 @@ function displayCart() {
 
 onLoadCartNumbers();
 displayCart()
+
+
+//PRODUCTY.HTML//
+
+$(document).ready(function() {
+    $.get('http://127.0.0.1:5500/eshop.json', function(response) {
+        $('#good').empty();
+        for (let product of response) {
+            console.log(product);
+            let newProduct = `
+            <div  class="goods">
+                <div class="tool-box">
+                <img src="${product.image}" alt="product_1">
+                    <a href=""></a>
+                </div>
+                    <div class="description">
+                        <p>${product.name}</p>
+                        <div class="values">
+                            <h4>${product.specialPrice}</h4>
+                            <span><del>${product.price}</del></span>
+                        </div>
+                    </div>
+                <div class="cart">
+                    <a class="add-cart">do košíka</a> <!--pridal som "add-cart" -pouzitie triedy v.js(vybratie vsetkych elementov pre ktory tento selektor plati)- na kazdy produkt na stranke domov-->
+                </div>
+            </div>`;
+            $('#good').append(newProduct);
+        }
+    });
+});
